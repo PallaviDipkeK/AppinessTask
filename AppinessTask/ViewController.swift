c472ef76c36a5491be00dc7385c4a20fb1d91992
Pankaj <pankaj.kumawat@mobiotics.com>
2020-09-22 17:23:35 +0530

    Initial Commit

---
+++ AppinessTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppinessTask
+//
+//  Created by mobiotics1067 on 22/09/20.
+//  Copyright © 2020 Private. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
